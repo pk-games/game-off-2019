a03b3297715e8a81b59c969a4663bf3f96c69e31
Mubean Hussain <mubean@park-it-solutions.com>
2019-11-08 18:03:05 +0000

    Initial scene setup and scripts

---
+++ Assets/Scripts/Button.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    private GameObject Door;
+    private Transform Target;
+
+    private void Start()
+    {
+        Target = GameObject.Find("Door").transform;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    { // If player is pressing the button, then open the door
+        if(other.gameObject.name=="Player")
+        {
+            StartCoroutine("OpenDoor");
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.name == "Player")
+        { //if player is off the button, stop opening the door
+           StopCoroutine("OpenDoor");
+        }
+    }
+
+    IEnumerator OpenDoor()
+    {
+        while(true)
+        {
+            Target.transform.Translate(0, Time.deltaTime, 0);
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public float FollowSpeed = 2f;
+    private Transform Target;
+    private GameObject Player;
+
+    private void Awake()
+    { // Find the Player in the scene and assign to the target variable. this makes the camera focus on the player.
+        Target = GameObject.Find("Player").transform;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPosition = Target.position;
+        newPosition.z = -5;
+        transform.position = Vector3.Slerp(transform.position, newPosition, FollowSpeed * Time.deltaTime);
+    }
+}
