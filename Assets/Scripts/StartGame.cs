39aec3525f2a9149005b40225d9a274b670d0737
Mubean Hussain <mubean@park-it-solutions.com>
2019-11-14 17:59:23 +0000

    Menu added
    Menu to add music and click sounds, followed with a transition fade out/in

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class StartGame : MonoBehaviour
+{
+    public AudioClip OnClickSound;
+    private int firstLevel = 1;
+    public Color loadToColor = Color.black;
+
+
+    public void OnClick()
+    {
+        StartCoroutine("LoadLevel");
+    }
+
+    IEnumerator LoadLevel()
+    {
+        Initiate.Fade("", loadToColor, 1.5f);
+        GetComponent<AudioSource>().PlayOneShot(OnClickSound);
+        yield return new WaitForSeconds(2.0f);
+        SceneManager.LoadScene(firstLevel);
+    }
+}
