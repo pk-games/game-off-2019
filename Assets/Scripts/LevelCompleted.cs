72b8db514e022e747e9a95636ade9c6f4a83c599
Mubean Hussain <mubean@park-it-solutions.com>
2019-11-15 10:36:57 +0000

    Add Prefab and script for level completion

---
+++ Assets/Scripts/LevelCompleted.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelCompleted : MonoBehaviour
+{
+	private int CurrentScene;
+	private Color loadToColor = Color.black;
+	void Start()
+    {
+		CurrentScene = SceneManager.GetActiveScene().buildIndex;
+    }
+
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		print(col.gameObject.name);
+		if (col.gameObject.name == "Player")
+		{
+			StartCoroutine("LoadNextLevel");
+		}
+	}
+
+	IEnumerator LoadNextLevel()
+	{// fade out and go to the next level
+		Initiate.Fade("", loadToColor, 1.5f);
+		yield return new WaitForSeconds(2.0f);
+		SceneManager.LoadScene(++CurrentScene);
+	}
+}
