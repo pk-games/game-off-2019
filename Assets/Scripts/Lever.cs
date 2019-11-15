ee675d12a675cb6de2a92ce9e8e88f54ce6ca6fc
Jamie Weavis <jamie.weavis@gmail.com>
2019-11-15 14:21:20 +0000

    Add interactive Lever prefab

---
+++ Assets/Scripts/Lever.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class Lever : MonoBehaviour
+{
+    public GameObject trigger;
+    private SpriteRenderer spriteRenderer;
+    private bool isInRange;
+    private bool isActive;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Interact") && isInRange)
+        {
+            isActive = !isActive;
+            spriteRenderer.flipX = isActive;
+
+            // TODO: Call method on trigger class
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            isInRange = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            isInRange = false;
+        }
+    }
+}
