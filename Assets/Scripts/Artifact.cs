e4bd49cfef19c12b8f7475fddece53927b0a6a65
Jamie Weavis <jamie.weavis@gmail.com>
2019-11-25 21:25:59 +0000

    Implement artifact functionality

---
+++ Assets/Scripts/Artifact.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class Artifact : MonoBehaviour
+{
+    public GameObject[] triggers;
+
+    private bool isInRange;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Interact") && isInRange)
+        {
+            ToggleTriggers();
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void ToggleTriggers()
+    {
+        for (int i = 0; i < triggers.Length; i++)
+        {
+            triggers[i].SetActive(!triggers[i].activeSelf);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            isInRange = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            isInRange = false;
+        }
+    }
+}
