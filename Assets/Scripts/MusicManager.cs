7885a492d40c00f8824d0889ad80986120d5b736
Jamie Weavis <jamie.weavis@gmail.com>
2019-11-26 19:12:10 +0000

    Enable music to continue throughout scenes

---
+++ Assets/Scripts/MusicManager.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public AudioClip music;
+
+    private static MusicManager _instance;
+
+    public static MusicManager instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = GameObject.FindObjectOfType<MusicManager>();
+                DontDestroyOnLoad(_instance.gameObject);
+            }
+
+            return _instance;
+        }
+    }
+
+    void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            if (this != _instance)
+            {
+                Destroy(this.gameObject);
+            }
+                
+        }
+        Play();
+    }
+
+    public void Play()
+    {
+        this.gameObject.GetComponent<AudioSource>().Play();
+    }
+}
