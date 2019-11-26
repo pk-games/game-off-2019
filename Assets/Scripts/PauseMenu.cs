c40b48724a774426b2d0a0a56f6227cf6cd24189
Jamie Weavis <jamie.weavis@gmail.com>
2019-11-26 20:38:37 +0000

    Add basic pause menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+	public static bool gameIsPaused;
+    public GameObject overlay;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+		{
+            if (gameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+		}
+    }
+
+    public void Resume()
+    {
+        overlay.SetActive(false);
+        Time.timeScale = 1;
+        gameIsPaused = false;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        Resume();
+    }
+
+    public void Quit()
+    {
+        SceneManager.LoadScene(0);
+        Resume();
+    }
+
+    void Pause()
+    {
+        overlay.SetActive(true);
+        Time.timeScale = 0;
+        gameIsPaused = true;
+    }
+}
