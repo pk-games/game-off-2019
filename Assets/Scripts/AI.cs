9b30a9398b1ee00d8085e6099f74d699e94290e2
Mubean Hussain <mubean@park-it-solutions.com>
2019-11-26 18:05:48 +0000

    Add AI script

---
+++ Assets/Scripts/AI.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class AI : MonoBehaviour
+{
+
+    private Transform target;
+    private float speed = 2f;
+    private bool ChasePlayer = false;
+
+    void Start()
+    {
+        target = GameObject.Find("Player").transform;
+
+    }
+
+    private void Update()
+    {
+        if(ChasePlayer)
+        {
+            transform.parent.LookAt(target.position);
+            transform.parent.Rotate(new Vector3(0, -90, 0), Space.Self);
+            transform.parent.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+        }
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            ChasePlayer = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            ChasePlayer = false;
+        }
+    }
+}
+ 
