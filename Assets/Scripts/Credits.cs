d129883c576e5c0e5ded744c523ffdf665a194b4
Mubean Hussain <mubean@park-it-solutions.com>
2019-11-28 18:52:22 +0000

    Added Credit Scene

---
+++ Assets/Scripts/Credits.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Credits : MonoBehaviour
+{
+    private void Start()
+    {
+        MusicManager.instance.PlayCalmMusic();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            StartCoroutine(LoadMenu());
+        }
+    }
+    IEnumerator LoadMenu()
+    {
+        Initiate.Fade("", Color.black, 1);
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(0);
+    }
+}
