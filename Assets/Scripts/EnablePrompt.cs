a773d4b8c3f31278c46167425b8c1823ff7748ac
Mubean Hussain <mubean@park-it-solutions.com>
2019-11-29 15:17:47 +0000

    Add prompts inni

---
+++ Assets/Scripts/EnablePrompt.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+
+
+public class EnablePrompt : MonoBehaviour
+{
+    public GameObject Text;
+    private void Update()
+    {
+       
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+            Text.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+            Text.SetActive(false);
+        }
+
+    }
+
+    void OnTriggerStay2D(Collider2D col)
+    {
+        if (col.gameObject.name == "Player")
+        {
+            Text.SetActive(true);
+        }
+    }
+}
